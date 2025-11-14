df65a1fe027d61a4b5b75aad370fd8282cf1c220
yamada1001 <78719289+yamada1001@users.noreply.github.com>
2025-11-15 01:54:23 +0900

    Revert "無印良品スタイルのGSAPアニメーション実装（安全版）"
    This reverts commit be3b1ac0111e8fcbac222c53777f59b2186d5dea.

--- index.php
+++ index.php
@@ -379,15 +379,6 @@ sort($all_categories);
         </div>
     </div>
 
-    <!-- GSAP 3.x Core Library -->
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- GSAP ScrollTrigger Plugin -->
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js" integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- GSAP Animations - Minimal & Refined -->
-    <script defer src="assets/js/animations.js"></script>
-
     <!-- JavaScript - Deferred for better performance -->
     <script defer src="assets/js/app.js"></script>
 </body>
