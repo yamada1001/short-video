eb83fd0c742e695e0cc6a8a71e43ff039121e9cf
yamada1001 <78719289+yamada1001@users.noreply.github.com>
2025-11-19 15:18:14 +0900

    Debug: 英語版コンテンツ読み込みエラーの原因調査

--- en/blog/detail.php
+++ en/blog/detail.php
@@ -31,6 +31,9 @@ if (isset($post['content']) && strpos($post['content'], '.html') !== false) {
         $post['content'] = processBlogContent($post['content']);
         // Translate content
         $post['content'] = translateContent($post['content'], $post['id'], $post['updatedAt']);
+    } else {
+        // Debug: show path information if file not found
+        $post['content'] = '<p style="color:red;">Debug: File not found</p><p>BASE_PATH: ' . BASE_PATH . '</p><p>Content file: ' . $content_file . '</p>';
     }
 }
 
