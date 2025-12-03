3c56983307f7789fd482ea59c6f21cde77507fae
yamada1001 <78719289+yamada1001@users.noreply.github.com>
2025-12-03 14:07:17 +0900

    Clean: デバッグコードとテストファイルを削除（本番用にクリーンアップ）

--- demo/kuruma-kaitori-k-village/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// シンプルなテストファイル
-echo "PHP Version: " . phpversion() . "<br>";
-echo "Current Directory: " . __DIR__ . "<br>";
-echo "File exists test:<br>";
-echo "- data/config.php: " . (file_exists(__DIR__ . '/data/config.php') ? 'YES' : 'NO') . "<br>";
-echo "- includes/header.php: " . (file_exists(__DIR__ . '/includes/header.php') ? 'YES' : 'NO') . "<br>";
-echo "<br>If you see this, PHP is working!";
